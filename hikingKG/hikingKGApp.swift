200109e1bded3ba3c628dc138f40132652d08941
jizzman1810 <jizzman1810@gmail.com>
2026-05-09 12:55:57 +0300

    Initial Commit

---
+++ hikingKG/hikingKGApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikingKGApp.swift
+//  hikingKG
+//
+//  Created by Александр Пакшин on 09.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct hikingKGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
